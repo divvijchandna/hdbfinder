ae381b3e95f5f251b68bf315b9ced4c9fca6ff9a
Divvij Chandna <divvijchandna@gmail.com>
2020-03-17 01:20:06 +0800

    Create sign_in.dart

---
+++ lib/models/sign_in.dart
@@ -0,0 +1,31 @@
+class SignIn{
+  String _facebookID;
+  String _gmail;
+  String _password;
+  String _email;
+
+  String get facebookID => _facebookID;
+
+  set facebookID(String value) {
+    _facebookID = value;
+  }
+
+  String get gmail => _gmail;
+
+  String get email => _email;
+
+  set email(String value) {
+    _email = value;
+  }
+
+  String get password => _password;
+
+  set password(String value) {
+    _password = value;
+  }
+
+  set gmail(String value) {
+    _gmail = value;
+  }
+
+}
