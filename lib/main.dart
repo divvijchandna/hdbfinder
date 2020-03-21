cb845228d094f8a9fcfb8ee480007cbc952a2fdd
mohitprashant <18mohitp@gmail.com>
2020-03-21 14:55:19 +0800

    up

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,27 @@ import 'package:flutter/material.dart';
 import 'package:hdbfinder/screens/nav-drawer.dart';
 import 'package:hdbfinder/screens/wrapper.dart';
 
-void main() => runApp(MyApp());
+import 'package:hdbfinder/services/search_request/data_request.dart';
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Wrapper(),
-    );
-  }
+void main() async{
+  print(1);
+  var f=FetchJSON();
+  await f.fetch(pollutionIndex);
+  print(f.jsonbody['items'][0]['readings']['o3_sub_index'][f.jsonbody['region_metadata'][0]['name']]);
+  print(2);
+
 }
+
+
+//=> runApp(MyApp());
+//
+//class MyApp extends StatelessWidget {
+//  // This widget is the root of your application.
+//  @override
+//  Widget build(BuildContext context) {
+//    return MaterialApp(
+//        home: Wrapper(),
+//    );
+//  }
+//}
 
