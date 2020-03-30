9843d5083fac9a1b41f56a6ebab0b9a230e521b0
Mac Milin <macmilinkiran@gmail.com>
2020-03-30 13:52:21 +0800

    formatting

--- lib/screens/home/hdb_settings.dart
+++ lib/screens/home/hdb_settings.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:hdbfinder/screens/home/saved_searches.dart';
 import 'package:hdbfinder/screens/home/home.dart';
+import 'package:hdbfinder/shared/drawer.dart';
+import 'package:google_fonts/google_fonts.dart';
+
 
 class Settings extends StatefulWidget {
   @override
@@ -11,57 +14,17 @@ class _SettingsState extends State<Settings> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: <Widget>[
-            DrawerHeader(
-              child: Text(
-                'HDBFinder Menu',
-                style: TextStyle(color: Color(0xff3a506b), fontSize: 25),
-              ),
-              decoration: BoxDecoration(
-                color: Color(0xffb5BAD0),
-              ),
-            ),
-            ListTile(
-              leading: Icon(Icons.home),
-              title: Text('Home'),
-              onTap: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => Home())
-                );
-              },
-            ),
-            ListTile(
-              leading: Icon(Icons.save_alt),
-              title: Text('Saved Searches'),
-              onTap: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => SavedSearches())
-                );
-                },
-            ),
-            ListTile(
-              leading: Icon(Icons.settings),
-              title: Text('Settings'),
-              onTap: () => {Navigator.of(context).pop()},
-            ),
-            ListTile(
-              leading: Icon(Icons.exit_to_app),
-              title: Text('Logout'),
-              onTap: () => {Navigator.of(context).pop()},
-            ),
-          ],
-        ),
-      ),
+      drawer: MenuDrawer(),
       backgroundColor: Color(0xffe0e0e2),
       appBar: AppBar(
-        backgroundColor: Color(0xff3a506b),
+        backgroundColor: Color(0xff003f91),
         centerTitle: true,
-        title: Text('Settings'),
+        title: Text('Settings',
+          style: GoogleFonts.montserrat(
+              textStyle: TextStyle(
+                  color: Color(0xffe5f4e3), fontSize: 22.0, fontWeight: FontWeight.w700)
+          ),
+        ),
       ),
     );
   }
