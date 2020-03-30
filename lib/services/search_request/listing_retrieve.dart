c3840359466fb759c09ecae981b47fa797672897
Mohit Prashant <32471894+mohitprashant@users.noreply.github.com>
2020-03-30 23:40:18 +0800

    Add files via upload

--- lib/services/search_request/listing_retrieve.dart
+++ lib/services/search_request/listing_retrieve.dart
@@ -5,7 +5,7 @@ import 'package:hdbfinder/models/apply_filters.dart';
 class SearchListings{
   String request=resalePrice;
   Filters filters;
-  String retrieveNumber='&limit=100';
+  String retrieveNumber='&limit=10';
   String sortBy='';
   FetchJSON listing=FetchJSON();
 
@@ -46,6 +46,54 @@ class SearchListings{
   }
 
   Future<void> getListingsByFilter() async{
+    int pricerange=((filters.filterRange.maxPrice-filters.filterRange.minPrice)/1000).round();
+    int arearange=filters.filterRange.maxArea-filters.filterRange.minArea+1;
+
+    List<String> queries=List(arearange*pricerange);
+
+    int k=0;
+
+    for(int i=filters.filterRange.minArea;i<=filters.filterRange.maxArea;i++){
+      for(int j=filters.filterRange.minPrice;j<filters.filterRange.maxPrice;j=j+1000){
+        String query='';
+        if(filters.filterByType==1){
+          query=query+',"flat_type":"'+filters.filterRange.type+'"';
+        }
+        if(filters.filterByModel==1){
+          query=query+',"flat_model":"'+filters.filterRange.model+'"';
+        }
+        if(filters.filterByTown==1){
+          query=query+',"town":"'+filters.filterRange.town+'"';
+        }
+        query=query+',"resale_price":'+'"'+j.toString()+'"';
+        query=query+',"floor_area_sqm":'+'"'+i.toString()+'"';
+        query=query.substring(1);
+
+        query=request+'&q={'+query+'}';
+        query=query+retrieveNumber;
+        query=query+sortBy;
+
+        queries[k]=query;
+        k++;
+      }
+    }
+    var tempListing=FetchJSON();
+
+    await listing.fetch(queries[0]);
+
+    for(int i=1;i<queries.length;i++){
+      await tempListing.fetch(queries[i]);
+
+      print(queries[i]);
+      print(tempListing.jsonBody['result']['records']);
+
+      listing.jsonBody['result']['records'].addAll(tempListing.jsonBody['result']['records']);
+    }
+
+    print(listing.jsonBody['result']['records']);
+  }
+
+  Future<void> getListingsByFilterdeprecate() async{
     String query='';
 
     if(filters.filterByPrice==1){
@@ -74,8 +122,11 @@ class SearchListings{
     request=request+sortBy;
 
     print(request);
+    print(request.length);
 
     await listing.fetch(request);
+
+    print(listing.jsonBody['result']['records']);
   }
 
   Future<void> getListingsByKeyword(String keyword) async{
